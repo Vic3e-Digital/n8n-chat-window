e3fb1858964392d8a1b00d0494aaa17e44ebe8ed
Vic3e.Digtial <opeolu.victory@gmail.com>
2025-04-02 17:21:35 +0200

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -422,7 +422,8 @@
 
             const botMessageDiv = document.createElement('div');
             botMessageDiv.className = 'chat-message bot';
-            botMessageDiv.textContent = Array.isArray(responseData) ? responseData[0].output : responseData.output;
+            // Replace \n with <br> for line breaks
+            botMessageDiv.innerHTML = (Array.isArray(responseData) ? responseData[0].output : responseData.output).replace(/\n/g, '<br>');
             messagesContainer.appendChild(botMessageDiv);
             messagesContainer.scrollTop = messagesContainer.scrollHeight;
         } catch (error) {
@@ -460,7 +461,8 @@
             
             const botMessageDiv = document.createElement('div');
             botMessageDiv.className = 'chat-message bot';
-            botMessageDiv.textContent = Array.isArray(data) ? data[0].output : data.output;
+            // Replace \n with <br> for line breaks
+            botMessageDiv.innerHTML = (Array.isArray(data) ? data[0].output : data.output).replace(/\n/g, '<br>');
             messagesContainer.appendChild(botMessageDiv);
             messagesContainer.scrollTop = messagesContainer.scrollHeight;
         } catch (error) {
